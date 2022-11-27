642c7caf64c48f28cfc2225daff32d170b3e9089
Mangel9701 <miguelangel97011997@gmail.com>
2022-11-27 11:36:20 -0500

    patricio_final

--- Assets/move.cs
+++ Assets/move.cs
@@ -18,9 +18,9 @@ Rigidbody rb; //Tells script there is a rigidbody, we can use variable rb to ref
     void Update()
     {
         float xMove = Input.GetAxisRaw("Horizontal"); // d key changes value to 1, a key changes value to -1
-float zMove = Input.GetAxisRaw("Vertical"); // w key changes value to 1, s key changes value to -1
+        float zMove = Input.GetAxisRaw("Vertical"); // w key changes value to 1, s key changes value to -1
 
-rb.velocity = new Vector3(xMove, 0, zMove) * speed; // Creates velocity in direction of value equal to keypress (WASD). rb.velocity.y deals with falling + jumping by setting velocity to y. 
+        rb.velocity = new Vector3(xMove, 0, zMove) * speed; // Creates velocity in direction of value equal to keypress (WASD). rb.velocity.y deals with falling + jumping by setting velocity to y. 
 
     }
 }
