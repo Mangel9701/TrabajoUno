9527ab2b552a3ebfb610f37834b344b40735a608
Trixtaro <wilmerdavid97@hotmail.com>
2022-11-29 00:28:51 -0500

    Creacion de menu

---
+++ Assets/Scripts/OpenScene.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class OpenScene : MonoBehaviour, IPointerClickHandler
+{
+    public string sceneName;
+    public void OnPointerClick(PointerEventData pointerEventData)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
