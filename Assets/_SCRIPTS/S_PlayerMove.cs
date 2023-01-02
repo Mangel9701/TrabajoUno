49849aca633bf881d0b696028fe182eba5c51f2e
Mangel9701 <miguelangel97011997@gmail.com>
2023-01-02 16:08:40 -0500

    final

--- Assets/_SCRIPTS/S_PlayerMove.cs
+++ Assets/_SCRIPTS/S_PlayerMove.cs
@@ -25,6 +25,7 @@ public class S_PlayerMove : MonoBehaviour
     [SerializeField] private float FuerzaSalto;
     public float VelocidadCaminar, VelocidadCorrer, gravedad, FactorVelocidad;
     private Vector3 VelocidadVertical, VelocidadHorizontal;
+    public bool Atacando;
 
     private float MovimientoMouse;
 
@@ -35,14 +36,14 @@ public class S_PlayerMove : MonoBehaviour
 
     [Header("Requeridos")]
     [Space]
-    public CharacterController CharacterController;
-    public AudioSource FuenteAudio;
-    public AudioClip SonidoMoneda;
-    public AudioSource SonidoPasos;
-    public Animator Animador;
-    public Image Corazon;
-    public TextMeshProUGUI TextoPuntuacion;
-    public Image[] Corazones;
+    [SerializeField] private CharacterController CharacterController;
+    [SerializeField] private AudioSource FuenteAudio;
+    [SerializeField] private AudioClip SonidoMoneda;
+    [SerializeField] private AudioSource SonidoPasos;
+    [SerializeField] private Animator Animador;
+    [SerializeField] private TextMeshProUGUI TextoPuntuacion;
+    [SerializeField] private Image[] Corazones;
+    [SerializeField] private GameObject CajaDano;
     public bool isLoadingPosition = false;
 
     private void Awake() {
@@ -64,103 +65,65 @@ public class S_PlayerMove : MonoBehaviour
             //SceneManager.LoadScene(0); indice segun el build scene
         }
 
-        #region Movimiento
+        Movimiento();
 
-        VelocidadHorizontal = Input.GetAxis("Vertical") * transform.forward;
-        
-        //delta time asegura que se mueva a la velocidad por segundo y no por frame
-        VelocidadVertical += Physics.gravity*gravedad; //*Time.deltaTime
-        
+        SonidoPasos.volume = Mathf.Abs(Input.GetAxis("Vertical"));
+        //la longitud de vertical siempre es de 1 a -1 absoluto = a 1 cuando nos movamos
 
-        MovimientoMouse = VelocidadRotacion * (Input.mousePosition.x - CentroPantalla.x);
-
-        if (MovimientoMouse <- MovimientoMouseMin || MovimientoMouse > MovimientoMouseMin)
+        if (Input.GetMouseButtonDown(0))
         {
-            transform.Rotate(new Vector3(0, MovimientoMouseMin * Mathf.Deg2Rad, 0));
-        }else
-        {
-            transform.Rotate(new Vector3(0, MovimientoMouse * Mathf.Deg2Rad, 0));
+            Atacar();
         }
 
-        if (Input.GetKey(KeyCode.LeftShift)|| Input.GetKey(KeyCode.RightShift))
+    }
+
+    private void Movimiento() 
+    {
+
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
         {
             VelocidadHorizontal *= VelocidadCorrer; //* Time.deltaTime
         }
         else
         {
-            VelocidadHorizontal  *= VelocidadCaminar;
+            VelocidadHorizontal *= VelocidadCaminar;
         }
 
-        CharacterController.Move( (VelocidadHorizontal + VelocidadVertical) * Time.deltaTime);
+        CharacterController.Move((VelocidadHorizontal + VelocidadVertical) * Time.deltaTime);
 
         //en la animacion has exit time es que ejecute la animacion tras haber terminado la anterior
         //magnitud es la longitud del vector(velocidadH)
-        Animador.SetFloat("Velocidad", VelocidadHorizontal.magnitude*FactorVelocidad); //* Time.deltaTime
+        Animador.SetFloat("Velocidad", VelocidadHorizontal.magnitude * FactorVelocidad); //* Time.deltaTime
 
         if (Input.GetKeyDown(KeyCode.Space) && CharacterController.isGrounded) //con el is grounded nos aseguramos que el personaje este tocando el piso 
         {
             VelocidadVertical = FuerzaSalto * Vector3.up; //mide uno hacia arriba en el eje Y
-            Animador.SetBool("Salto",true);
+            Animador.SetBool("Salto", true);
 
-        }else if (CharacterController.isGrounded) 
+        }
+        else if (CharacterController.isGrounded)
         {
             Animador.SetBool("Salto", false);//si no esta saltando pero si en el piso
         }
 
-        #endregion
+        VelocidadHorizontal = Input.GetAxis("Vertical") * transform.forward;
 
-        #region Ejemplos
-
-        /*
-        Puntaje++;
-        Puntaje += 1;
-        Puntaje = Puntaje + 1;
-
-        Puntaje--;
-        Puntaje -= 1;
-
-        Puntaje *= 2;
-        Puntaje /= 2;
-
-        EstaVivo = false;
-        EstaVivo = true;
-
-        Nombre = "Irene";
-        
-         //transform.localPosition = MovimientoHorizontal;
-        //transform.localPosition = new Vector3(1,0,0);
-        //transform.Position += new Vector3(1,0,0);
-        //transform.Position += new Vector3(10,0,0);
-         
-         if (Input.GetKey(KeyCode.W)) 
-        { 
-            transform.position += transform.forward * Velocidad * Time.deltaTime + VelocidadVertical; 
-        }
-         
-         */
+        //delta time asegura que se mueva a la velocidad por segundo y no por frame
+        VelocidadVertical += Physics.gravity * gravedad; //*Time.deltaTime
 
 
-        //el get axis da desde 1 a -1, 1=delante 0=nada -1=atras
-        // este ya no es necesario por el uso del Character Controler transform.position += Input.GetAxis("Vertical")*transform.forward * Velocidad * Time.deltaTime + VelocidadVertical;
+        MovimientoMouse = VelocidadRotacion * (Input.mousePosition.x - CentroPantalla.x);
 
-
-        //float MovimientoMouse = Input.mousePosition.x - CentroPantalla.x;
-
-        /*if (MovimientoMouse<MovimientoMouseMin || MovimientoMouse>MovimientoMouseMin)
+        if (MovimientoMouse < -MovimientoMouseMin || MovimientoMouse > MovimientoMouseMin)
         {
-            transform.Rotate(new Vector3(0, VelocidadRotacion * MovimientoMouse, 0));
-        }*/
+            transform.Rotate(new Vector3(0, MovimientoMouseMin * Mathf.Deg2Rad, 0));
+        }
+        else
+        {
+            transform.Rotate(new Vector3(0, MovimientoMouse * Mathf.Deg2Rad, 0));
+        }
 
-        //public Collider Collider; no se usa porque el controller ya viene con uno
-        //no permite usar el rigidbody
-        //public Rigidbody RB;
-
-
-
-        #endregion
-
-        SonidoPasos.volume = Mathf.Abs(Input.GetAxis("Vertical"));
-        //la longitud de vertical siempre es de 1 a -1 absoluto = a 1 cuando nos movamos
+        
 
     }
 
@@ -202,5 +165,71 @@ public class S_PlayerMove : MonoBehaviour
         } 
     }
 
-    
+    void Atacar()
+    {
+        CajaDano.SetActive(true);
+        Invoke("FinAtaque", 0.5f);
+        Atacando = true;
+        Animador.SetBool("Atacando",true);
+    }
+
+    void FinAtaque()
+    {
+        CajaDano.SetActive(false); 
+        Animador.SetBool("Atacando", false);
+        Atacando = false;
+
+    }
+
+    #region Ejemplos
+
+    /*
+    Puntaje++;
+    Puntaje += 1;
+    Puntaje = Puntaje + 1;
+
+    Puntaje--;
+    Puntaje -= 1;
+
+    Puntaje *= 2;
+    Puntaje /= 2;
+
+    EstaVivo = false;
+    EstaVivo = true;
+
+    Nombre = "Irene";
+
+     //transform.localPosition = MovimientoHorizontal;
+    //transform.localPosition = new Vector3(1,0,0);
+    //transform.Position += new Vector3(1,0,0);
+    //transform.Position += new Vector3(10,0,0);
+
+     if (Input.GetKey(KeyCode.W)) 
+    { 
+        transform.position += transform.forward * Velocidad * Time.deltaTime + VelocidadVertical; 
+    }
+
+     */
+
+
+    //el get axis da desde 1 a -1, 1=delante 0=nada -1=atras
+    // este ya no es necesario por el uso del Character Controler transform.position += Input.GetAxis("Vertical")*transform.forward * Velocidad * Time.deltaTime + VelocidadVertical;
+
+
+    //float MovimientoMouse = Input.mousePosition.x - CentroPantalla.x;
+
+    /*if (MovimientoMouse<MovimientoMouseMin || MovimientoMouse>MovimientoMouseMin)
+    {
+        transform.Rotate(new Vector3(0, VelocidadRotacion * MovimientoMouse, 0));
+    }*/
+
+    //public Collider Collider; no se usa porque el controller ya viene con uno
+    //no permite usar el rigidbody
+    //public Rigidbody RB;
+
+
+
+    #endregion
+
+
 }
